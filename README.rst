9918fdbc89f9f72eb31eb4a290fa78f6ddf17ac2
Socketubs <geoffrey@lehee.name>
2012-09-08 02:47:50 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+4Chandownloader
+===============
+
+4chan thread downloader.
+
+```
+4chandownloader http://boards.4chan.org/b/res/423861837 4chanarchives --delay 5 --thumbs
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+		name             = '4chandownloader',
+		version          = '0.1',
+		description      = '4chan thread downloader',
+		long_description = open('README.rst').read(), 
+		license          = open('LICENSE').read(),
+		author           = 'Socketubs',
+		author_email     = 'geoffrey@lehee.name',
+		url              = 'https://github.com/Socketubs/4chandownloader',
+		keywords         = '4chan downloader images',
+		scripts          = ['4chandownloader'],
+		install_requires = ['requests==0.14.0', 'docopt==0.5.0'],
+		classifiers      = (
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7')
+)
